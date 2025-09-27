8530dce69b6855c81d41412256d4d0051f29a707
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-09-27 11:02:00 -0300

    feat: create HomePageSection Component

---
+++ src/components/HomePageSection.tsx
@@ -0,0 +1,20 @@
+interface HomePageSectionProps {
+   hasCtaElement?: boolean;
+   children: React.ReactNode;
+   className?: string;
+}
+
+export function HomePageSection({ children, hasCtaElement = false, className }: HomePageSectionProps) {
+   return (
+      <section className={`min-h-[calc(100vh-110px)] ${className}`}>
+         {hasCtaElement && (
+            <div className="flex justify-center items-center">
+               <button className="bg-foreground-secondary text-white px-4 py-2 rounded-md">
+                  CTA
+               </button>
+            </div>
+         )}
+         {children}
+      </section>
+   )
+}
