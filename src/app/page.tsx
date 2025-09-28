b9f69365d6befb890262c6599bb48ed743dd1719
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-09-28 15:23:01 -0300

    feat: add translated dynamic content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { HomePageSection } from "@/components/HomePageSection";
 import { ButtonCTA } from "@/components/ui/ButtonCTA";
 import { Arrow } from "@/components/ui/Arrow";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+   const t = useTranslations('home');
+
    return (
       <HomePageSection className="flex flex-col gap-10 items-center justify-center">
          <div className="flex items-center justify-center">
             <h1 className="w-full md:max-w-[45rem] text-6xl md:text-8xl text-center font-display">
-               We build brands with <b>flavor, strategy, and presence.</b>
+               {t("title.normalWeight")} <b>{t("title.boldWeight")}</b>
             </h1>
          </div>
          <ButtonCTA className="text-2xl" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 export function Header() {
+   const t = useTranslations('home');
+
+   const links = [
+      { href: '/about', label: t('about') },
+      { href: '/cases', label: t('cases') },
+      { href: '/contact', label: t('contact') },
+   ]
+   
    return (
       <header className="w-full px-8 h-[110px] flex justify-between items-center">
          <Image src="/logo.svg" alt="Pipo Studio" width={100} height={100} />
          <nav>
             <ul className="flex gap-4 text-foreground-secondary">
-               <Link href="/about">about.</Link>
-               <Link href="/cases">cases.</Link>
-               <Link href="/contact">contact.</Link>
+               {links.map((link) => (
+                  <Link href={link.href} key={link.label}>{link.label}</Link>
+               ))}
             </ul>
          </nav>
       </header>
