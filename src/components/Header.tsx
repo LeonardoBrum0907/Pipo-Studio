e87cda9a440936ca7a141ffba7e5afcef2fe50d9
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-09-28 16:56:08 -0300

    feat: create LocaleSwitcher Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+import { LocaleSwitcher } from "./LocaleSwitcher";
+import { Separator } from "./ui/Separator";
 
 export function Header() {
    const t = useTranslations('home');
 
    const links = [
-      { href: '/about', label: t('about') },
+      { href: '/', label: t('about') },
       { href: '/cases', label: t('cases') },
       { href: '/contact', label: t('contact') },
    ]
-   
+
    return (
       <header className="w-full px-8 h-[110px] flex justify-between items-center">
          <Image src="/logo.svg" alt="Pipo Studio" width={100} height={100} />
-         <nav>
+         <nav className="flex items-center gap-4 h-5">
             <ul className="flex gap-4 text-foreground-secondary">
                {links.map((link) => (
-                  <Link href={link.href} key={link.label}>{link.label}</Link>
+                  <Link href={link.href} key={link.label} className="border-b-2 border-transparent hover:border-foreground-secondary">{link.label}</Link>
                ))}
             </ul>
+            <Separator orientation="vertical" />
+            <LocaleSwitcher />
          </nav>
       </header>
 
