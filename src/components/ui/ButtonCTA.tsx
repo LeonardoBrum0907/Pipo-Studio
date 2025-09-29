9a7a3334ae907c405a8c38a9fa1f209efe52b706
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-09-28 21:32:38 -0300

    style: add hover to the ButtonCTA

--- src/components/ui/ButtonCTA.tsx
+++ src/components/ui/ButtonCTA.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 interface ButtonCTAProps {
    children?: React.ReactNode;
@@ -9,8 +10,8 @@ interface ButtonCTAProps {
 export function ButtonCTA({ children, type = "primary", className }: ButtonCTAProps) {
    const t = useTranslations('home');
    return (
-      <button className={`px-8 py-4 rounded-full ${type === "primary" ? "bg-foreground-secondary text-white" : "bg-transparent border border-foreground-secondary text-foreground-secondary"} ${className}`}>
+      <Link href="/contact" className={`px-8 py-4 rounded-full hover:opacity-90 transition-opacity ${type === "primary" ? "bg-foreground-secondary text-white" : "bg-transparent border border-foreground-secondary text-foreground-secondary"} ${className}`}>
          {children || t("cta")}
-      </button>
+      </Link>
    )
 }
