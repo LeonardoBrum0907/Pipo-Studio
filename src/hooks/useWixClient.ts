61c3611021eb656fc0b68f54e836596912e5bd04
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-10-01 11:59:30 -0300

    feat: create domain layer and add cases

--- src/hooks/useWixClient.ts
+++
@@ -1,9 +0,0 @@
-// import { createClient, OAuthStrategy } from '@wix/sdk';
-// import { items } from '@wix/data';
-// export const getWixClient = async () => {
-//   const wixClient = createClient({
-//     modules: { items },
-//     auth: OAuthStrategy({ clientId: '06c751ce-2aab-4cb1-97bb-7d9eb22225d0' }),
-//   });
-//   return wixClient;
-// };
