bbed7b56effd932ee1a3233a0ef9d1a99e12b569
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-10-01 11:57:18 -0300

    chore: add wix images config to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+   images: {
+      remotePatterns: [{ hostname: 'static.wixstatic.com' }],
+      formats: ['image/avif', 'image/webp'],
+   },
+   serverExternalPackages: ['@wix/sdk', '@wix/data'],
+};
 
 const withNextIntl = createNextIntlPlugin();
 
