4fbeb9782b08593e3baa6413b1f77f6e19e67fe4
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-10-03 12:19:15 -0300

    style: change height and font size on mobile

--- src/components/OurBrandingProcess.tsx
+++ src/components/OurBrandingProcess.tsx
@@ -17,12 +17,12 @@ export function OurBrandingProcess({ title, description }: OurBrandingProcessPro
    }
 
    return (
-      <div className={`w-full md:max-w-2/3 mx-auto flex flex-col gap-8 border-b border-foreground not-last:mb-12 cursor-pointer ${expandedBrandingProcessList ? 'h-[200px] pb-12' : 'h-0 pb-16'} transition-all duration-300 overflow-hidden`} onClick={toggleBrandingProcessList}>
+      <div className={`w-full md:max-w-2/3 mx-auto flex flex-col gap-8 border-b border-foreground not-last:mb-12 cursor-pointer ${expandedBrandingProcessList ? 'h-[180px] md:h-[200px] pb-12' : 'h-0 pb-16'} transition-all duration-300 overflow-hidden`} onClick={toggleBrandingProcessList}>
          <div className="flex items-center gap-4">
             <Arrow isOpen={expandedBrandingProcessList} />
-            <h2 className="text-4xl font-display">{title}</h2>
+            <h2 className="text-2xl md:text-4xl font-display">{title}</h2>
          </div>
-         <p className="text-2xl">
+         <p className="md:text-2xl">
             {description}
          </p>
       </div>
