6f66abcb026bad7e3c4bb9905ce3c516a50e0311
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-10-08 12:13:43 -0300

    style: add padding top to the HomePageSection Component

--- src/components/HomePageSection.tsx
+++ src/components/HomePageSection.tsx
@@ -9,7 +9,7 @@ interface HomePageSectionProps {
 
 export function HomePageSection({ children, hasCTAElement = false, className }: HomePageSectionProps) {
    return (
-      <section className={`min-h-[calc(100vh-110px)] px-8 md:px-16 not-first:mt-16 flex flex-col gap-8`}>
+      <section className={`min-h-[calc(100vh-110px)] px-8 md:px-16 py-8 not-first:mt-16 flex flex-col gap-8`}>
          {hasCTAElement && (
             <div className="flex justify-between items-center">
                <Image src="/logo.svg" alt="Pipo Studio" width={100} height={100} />
