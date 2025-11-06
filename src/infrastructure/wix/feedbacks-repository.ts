4382858787542a15f126eb195848b4fc480ed363
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-11-05 22:12:50 -0300

    feat: implement WixFeedbacksRepository for fetching customer feedback from Wix

---
+++ src/infrastructure/wix/feedbacks-repository.ts
@@ -0,0 +1,38 @@
+import { FeedbacksRepository } from "@/domain/feedbacks/repository";
+import { Feedback } from "@/domain/feedbacks/types";
+import { getWixClient } from "./client";
+
+interface WixFeedbackDTO {
+  customerName: string;
+  feedbackTextEn: string;
+  feedbackTextPt: string;
+  companyName: string;
+  avatar: string;
+  _id: string;
+}
+
+const mapWixFeedbackToFeedback = (item: WixFeedbackDTO): Feedback => {
+  return {
+    id: item._id,
+    avatarUrl: item.avatar,
+    customerName: item.customerName,
+    feedbackTextEn: item.feedbackTextEn,
+    feedbackTextPt: item.feedbackTextPt,
+    companyName: item.companyName,
+  };
+};
+
+export class WixFeedbacksRepository implements FeedbacksRepository {
+  private readonly collectionName = 'CustomerFeedbacks';
+
+  async findAll(): Promise<Feedback[]> {
+    const wixClient = await getWixClient();
+
+    const { items } = await wixClient.items.query(this.collectionName).find();
+
+   //  console.log('items', items);
+
+    return (items as WixFeedbackDTO[]).map(mapWixFeedbackToFeedback);
+  }
+}
+
